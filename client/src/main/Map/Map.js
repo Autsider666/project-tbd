90fe0ddcbdc94a7bea892628aa46a283ea1fea6e
Vincent Rye <opusvrye@gmail.com>
2022-07-23 20:28:55 +0200

    Updated Map Polygons

--- client/src/main/Map/Map.js
+++ client/src/main/Map/Map.js
@@ -12,7 +12,7 @@ const Map = () => {
     return (
         <>
             <TestSvg />
-            {/* <img style={{ opacity: 0.3, marginBottom: '-3px' }} src={Water} position="absolute" width="100%" /> */}
+            <img style={{ opacity: 0.3, marginBottom: '-3px' }} src={Water} position="absolute" width="100%" />
         </>
     )
 }
