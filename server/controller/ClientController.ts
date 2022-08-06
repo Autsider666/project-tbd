d657da9a03fd2a63f01f845baa9bdc8671baeb5a
Yorick <yorickvanklinken@gmail.com>
2022-08-06 20:18:46 +0200

    Refactoring code

--- server/controller/ClientController.ts
+++ server/controller/ClientController.ts
@@ -17,6 +17,11 @@ import {
 	ClientNotifier,
 	NotificationCategory,
 } from '../helper/ClientNotifier.js';
+import {
+	dismissSurvivor,
+	recruitSurvivor,
+	upgradeSurvivor,
+} from '../helper/SurvivorManagement.js';
 import { TravelTimeCalculator } from '../helper/TravelTimeCalculator.js';
 import { ExpeditionRepository } from '../repository/ExpeditionRepository.js';
 import { PartyRepository } from '../repository/PartyRepository.js';
@@ -370,154 +375,17 @@ export class ClientController {
 	}
 
 	private handleSurvivorManagement(): void {
-		this.socket.on(
-			'survivor:upgrade',
-			({ partyId, currentType, targetType }) => {
-				if (!(currentType in Survivor)) {
-					ClientNotifier.error(
-						`"${currentType}" is not a valid Survivor type.`,
-						this.socket.id
-					);
-					return;
-				}
-
-				if (!(targetType in Survivor)) {
-					ClientNotifier.error(
-						`"${targetType}" is not a valid Survivor type.`,
-						this.socket.id
-					);
-					return;
-				}
-
-				const currentSurvivor = SurvivorDataMap[currentType];
-				const upgradeCost = currentSurvivor.nextUpgradeCost;
-				if (
-					!currentSurvivor.upgrades.includes(targetType) ||
-					upgradeCost === -1
-				) {
-					ClientNotifier.error(
-						`Survivor "${currentType}" cannot be upgrade into "${targetType}".`,
-						this.socket.id
-					);
-					return;
-				}
-
-				const party = this.validatePartyForActivity(partyId);
-				if (party === null) {
-					return;
-				}
-
-				if (upgradeCost > party.energy) {
-					ClientNotifier.error(
-						`Party "${party.name}" is ${Math.abs(
-							party.energy - upgradeCost
-						)} energy short to upgrade this survivor.`,
-						party.getUpdateRoomName()
-					);
-
-					return;
-				}
-
-				if (!party.removeSurvivor(currentType)) {
-					ClientNotifier.error(
-						`There are no available ${currentType} in party "${party.name}".`,
-						party.getUpdateRoomName()
-					);
-				}
-
-				party.energy -= upgradeCost;
-
-				party.addSurvivor(targetType);
-			}
+		this.socket.on('survivor:upgrade', (data) =>
+			upgradeSurvivor(data, this.socket.id)
 		);
 
-		this.socket.on('survivor:recruit', ({ partyId, type }) => {
-			if (!(type in Survivor)) {
-				ClientNotifier.error(
-					`"${type}" is not a valid Survivor type.`,
-					this.socket.id
-				);
-				return;
-			}
+		this.socket.on('survivor:recruit', (data) =>
+			recruitSurvivor(data, this.socket.id)
+		);
 
-			const party = this.validatePartyForActivity(partyId);
-			if (party === null) {
-				return;
-			}
-
-			if (party.getSurvivors().length >= this.maxPartySize) {
-				ClientNotifier.error(
-					`Party "${party.name}" can't do this because it is already at the limit of ${this.maxPartySize} survivors.`,
-					party.getUpdateRoomName()
-				);
-
-				return null;
-			}
-
-			const settlement = party.getSettlement();
-			for (const survivor of settlement.getSurvivors()) {
-				if (survivor !== type) {
-					continue;
-				}
-
-				settlement.transferSurvivorTo(survivor, party);
-
-				ClientNotifier.success(
-					`${Survivor[type]} has been added to party "${party.name}"`,
-					party.getUpdateRoomName()
-				);
-
-				return;
-			}
-
-			ClientNotifier.error(
-				`There are no available ${type} in settlement "${settlement.name}".`,
-				party.getUpdateRoomName()
-			);
-		});
-
-		this.socket.on('survivor:dismiss', ({ partyId, type }) => {
-			if (!(type in Survivor)) {
-				ClientNotifier.error(
-					`"${type}" is not a valid Survivor type.`,
-					this.socket.id
-				);
-				return;
-			}
-
-			const party = this.validatePartyForActivity(partyId, false);
-			if (party === null) {
-				return;
-			}
-
-			const survivors = party.getSurvivors();
-			if (survivors.length === 1) {
-				ClientNotifier.warning(
-					`Cannot remove the last survivor of party "${party.name}`,
-					party.getUpdateRoomName()
-				);
-				return;
-			}
-
-			for (const survivor of survivors) {
-				if (survivor !== type) {
-					continue;
-				}
-				party.transferSurvivorTo(survivor, party.getSettlement());
-
-				ClientNotifier.success(
-					`${Survivor[type]} has left party "${party.name}"`,
-					party.getUpdateRoomName()
-				);
-
-				return;
-			}
-
-			ClientNotifier.error(
-				'This survivor is not accessible to this party.',
-				party.getUpdateRoomName()
-			);
-		});
+		this.socket.on('survivor:dismiss', (data) =>
+			dismissSurvivor(data, this.socket.id)
+		);
 	}
 
 	private handleSettlementManagement() {
