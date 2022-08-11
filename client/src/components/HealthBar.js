04713a9d24076efc84a51a53bdb25d9f4a71c583
Yorick <yorickvanklinken@gmail.com>
2022-08-11 22:45:08 +0200

    Math is not a function

--- client/src/components/HealthBar.js
+++ client/src/components/HealthBar.js
@@ -5,7 +5,7 @@ const HealthBar = ({ maxValue = 100, currentValue = 50, flip = false }) => {
 
 
     return <>
-        {flip && <Typography sx={{mr:1}}>{`${Math.ceil(currentValue)} / ${Math(maxValue)}`}</Typography>}
+        {flip && <Typography sx={{mr:1}}>{`${Math.ceil(currentValue)} / ${Math.ceil(maxValue)}`}</Typography>}
         <LinearProgress sx={{ width: '20%', height: 20, borderRadius: 1, transform: flip ? 'rotate(180deg)' : 'rotate(0deg)' }} variant="determinate" value={
             (currentValue / maxValue) * 100
             // normalise(buildings[upgrade.type].upgradeCost.amount - upgrade.remainingWork, 0, 100) / 100
